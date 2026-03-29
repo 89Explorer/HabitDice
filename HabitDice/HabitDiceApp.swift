640725b8a46f9afeb3bcc5d2b2548262b8c88167
89Explorer <leral3@naver.com>
2026-03-29 11:36:01 +0900

    Initial Commit

---
+++ HabitDice/HabitDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitDiceApp.swift
+//  HabitDice
+//
+//  Created by kwonjungguen on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct HabitDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitDiceTests/HabitDiceTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitDiceTests.swift
+//  HabitDiceTests
+//
+//  Created by kwonjungguen on 3/29/26.
+//
+
+import Testing
+@testable import HabitDice
+
+struct HabitDiceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
