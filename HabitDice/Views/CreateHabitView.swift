fceca69428c48e6fe8818e4283b19d00e299d615
89Explorer <leral3@naver.com>
2026-03-30 10:27:29 +0900

    Feat: HeaderTitleView 기본 구조 구현

--- HabitDice/Views/CreateHabitView.swift
+++ HabitDice/Views/CreateHabitView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct CreateHabitView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 8) {
+            HeaderView()
+        }
+    }
+    
+    @ViewBuilder
+    func HeaderView() -> some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text("오늘의 습관")
+                .font(.title.bold())
+            Text("2026년 3월 29일")
+                .font(.callout)
+                .fontWeight(.semibold)
+                .textScale(.secondary)
+                .foregroundStyle(.gray)
+        }
     }
 }
 
